98d2d55e53c561fd996f87ecf91bcf15885087c5
John Green <jpgreen@Johns-iMac.local>
2018-07-21 21:52:43 -0400

    Part One

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colorView.backgroundColor = .black
     }
 
     override func didReceiveMemoryWarning() {
